7d7e5da255dbf6f07af6a1b3a87b854c00ab02a3
Julio Flores <cesarff.julio@gmail.com>
2025-11-23 12:14:42 -0600

    build: :building_construction: estructura
    organizando la estructura para el manegador de formularios

---
+++ lib/src/index.jsx
@@ -0,0 +1,13 @@
+export { default as FormManager } from "./FormManger";
+export { default as useFormManager } from "./useFormManger";
+
+// Components
+export { default as FormProvider } from "./components/FormProvider";
+export { default as Field } from "./components/Field";
+export { default as FileField } from "./components/FileField";
+export { default as StepperWrapper } from "./components/StepperWrapper";
+export { default as SubmitButton } from "./components/SubmitButton";
+
+// Utils
+export * from "./utils/helpers";
+export * from "./utils/validation";
